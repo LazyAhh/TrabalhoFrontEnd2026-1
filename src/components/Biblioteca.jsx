28a3761ccc28c546d5a219885d5b41cf5d6cea33
Luiz Henrique <luizhenriquepimentel2005@gmail.com>
2026-03-30 16:05:23 -0300

    Incluindo nova funcionalidade

--- src/components/Biblioteca.jsx
+++ src/components/Biblioteca.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
+import livros from "../Data";
+import CaixaPesquisa from "./CaixaPesquisa";
+import BotoesFiltro from "./BotoesFiltro";
 import Card from "./Card";
 
 const Biblioteca = () =>{
-    
+    const [busca, setBusca] = useState('');
+    const [generoAtivo, setGeneroAtivo] = useState('');
+
+    const livrosFiltrados = livros.filter((livro) =>{
+        const correspondeNome = livros.nome.toLocaleLoweCase().includes(toLocaleLoweCase());
+        const correspondeGenero = generoAtivo === 'Todos' || livro.genero === generoAtivo;
+    })
 
     return(
-        <div>
-            <p>Biblioteca em andamento</p>
+        <div clssName="h-screen w-full flex flex-col bg-gray-100 p-6 overflow-hidden">
+            <div className="flex felx-col gap-4 mb-8">
+                <CaixaPesquisa busca={busca} setBusca={setBusca} />
+                <BotoesFiltro setGeneroAtivo={setGeneroAtivo} generoAtivo={generoAtivo} />
+            </div>
+
+            <div clssName="flex flex-1 items-center overflow-x-auto pb-4 gap-6 custom-scrollbar">
+                {livrosFiltrados.map((livros) =>(<Card Key={livro.id} livros={livros}/>))}
+            </div>
         </div>
     )
 }
 
-export default Biblioteca
+export default Lista;
