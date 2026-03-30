e0d16137ec7be4d1313279ed99f92a2a464dff61
DS <1-25-13173@aluno.faminas.edu.br>
2026-03-30 14:47:26 -0300

    Criação de todos os arquivos necessários para o Card e inclusão de Data.js

---
+++ src/components/Biblioteca.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Card from "./Card";
+
+const Biblioteca = () =>{
+    
+
+    return(
+        <div>
+            <p>Biblioteca em andamento</p>
+        </div>
+    )
+}
+
+export default Biblioteca
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Card = () =>{
+
+
+    return(
+        <div>
+            <p>Card em andamento...</p>
+        </div>
+    )
+}
+
+export default Card;
