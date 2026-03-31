471697166a77bfdd30186136a148d43068b658f5
Luiz Henrique <luizhenriquepimentel2005@gmail.com>
2026-03-31 12:57:19 -0300

    Adicionando comentarios

--- src/components/Biblioteca.jsx
+++ src/components/Biblioteca.jsx
@@ -5,23 +5,28 @@ import BotoesFiltro from "./BotoesFiltro";
 import Card from "./Card";
 
 const Biblioteca = () =>{
+
+    // Estado que armazena o texto digitado na caixa de pesquisa
     const [busca, setBusca] = useState('');
+     // Estado que armazena o gênero selecionado no filtro (ex: "Romance", "Ficção", etc.)
     const [generoAtivo, setGeneroAtivo] = useState('');
 
+     // Filtra a lista de livros com base no texto digitado e no gênero selecionado
     const livrosFiltrados = livros.filter((livro) =>{
         const correspondeNome = livros.nome.toLocaleLoweCase().includes(toLocaleLoweCase());
+     // Verifica se o gênero ativo é "Todos" (sem filtro) ou se o livro pertence ao gênero selecionado
         const correspondeGenero = generoAtivo === 'Todos' || livro.genero === generoAtivo;
     })
 
     return(
         <div clssName="h-screen w-full flex flex-col bg-gray-100 p-6 overflow-hidden">
-            <div className="flex felx-col gap-4 mb-8">
+            <div className="flex flex-col gap-4 mb-8">
                 <CaixaPesquisa busca={busca} setBusca={setBusca} />
                 <BotoesFiltro setGeneroAtivo={setGeneroAtivo} generoAtivo={generoAtivo} />
             </div>
 
             <div clssName="flex flex-1 items-center overflow-x-auto pb-4 gap-6 custom-scrollbar">
-                {livrosFiltrados.map((livros) =>(<Card Key={livro.id} livros={livros}/>))}
+                {livrosFiltrados.map((livros) =>(<Card key={livro.id} livros={livros}/>))}
             </div>
         </div>
     )
