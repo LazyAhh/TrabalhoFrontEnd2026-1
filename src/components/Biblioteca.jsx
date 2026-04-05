4bd865a8dff21e75ca98dc1e06659f5639929ce6
DS <1-25-13173@aluno.faminas.edu.br>
2026-04-05 15:58:22 -0300

    Lógica de filtragem adicionada na Biblioteca

--- src/components/Biblioteca.jsx
+++ src/components/Biblioteca.jsx
@@ -1,30 +1,26 @@
 import livros from "../Data";
-// import { useState } from "react";
-// import CaixaPesquisa from "./CaixaPesquisa";
-// import BotoesFiltro from "./BotoesFiltro";
+import { useState } from "react";
+import BotoesFiltro from "./BotoesFiltro";
 import Card from "./Card";
 
 const Biblioteca = () =>{
 
-    // const [busca, setBusca] = useState('');
-    // const [generoAtivo, setGeneroAtivo] = useState('');
+    const [generoAtivo, setGeneroAtivo] = useState('Todos');
 
-    // const livrosFiltrados = livros.filter((livro) =>{
-    //     const correspondeNome = livros.nome.toLocaleLoweCase().includes(toLocaleLoweCase());
-    //     const correspondeGenero = generoAtivo === 'Todos' || livro.genero === generoAtivo;
+    const livrosFiltrados = livros.filter((livro) =>{
+        const correspondeGenero = generoAtivo === 'Todos' || livro.genero === generoAtivo;
 
-    //     return correspondeGenero && correspondeNome
-    // })
+       return correspondeGenero;
+     })
 
     return(
         <div className="h-screen w-full flex flex-col bg-gray-100 p-6 overflow-hidden">
-            {/* <div className="flex flex-col gap-4 mb-8">
-                <CaixaPesquisa busca={busca} setBusca={setBusca} />
+            <div className="flex flex-col gap-4 mb-8">
                 <BotoesFiltro setGeneroAtivo={setGeneroAtivo} generoAtivo={generoAtivo} />
-            </div> */}
+            </div>
 
             <div className="flex flex-1 items-center overflow-x-auto pb-4 gap-6 custom-scrollbar">
-                {livros.map((livro) =>(<Card key={livro.id} livro={livro}/>))}
+                {livrosFiltrados.map((livro) =>(<Card key={livro.id} livro={livro}/>))}
             </div>
         </div>
     )
