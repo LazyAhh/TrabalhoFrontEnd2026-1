68b10674ea6a7487cbfff7ee2131fd1eb7274a0c
FernandaAllo <fernandasouzalimaallo@gmail.com>
2026-04-15 20:40:12 -0300

    componente das informações do card

---
+++ src/components/CardInformacoes.jsx
@@ -0,0 +1,21 @@
+
+const CardInformacoes = ({nome, autor, paginas, descricao}) =>{
+    return(
+        <>
+        <h2 className="text-xl font-bold text-gray-800 leading-tight">{nome || "Nenhum nome disponível"}</h2>
+                <p className="text-sm text-gray-800 italic mb-2">por {autor || "nennhum autor disponível"}</p>
+
+                <div className="text-xs text-blue-500 font-semibold mb-3">
+                    {paginas || "Sem número de "} páginas
+                </div>
+
+                <div className="flex-1 overflow-y-auto pr-2 custom-scrollbar">
+                    <p className="text-gray-600 text-sm pr-2 overflow-y-auto custom-scrollbar leading-relaxed">
+                        {descricao || "Nenhuma descrição disponível"}
+                    </p>
+        </div>
+        </>
+    )
+}
+
+export default CardInformacoes
