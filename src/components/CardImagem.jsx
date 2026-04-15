d89fc20857414bad151a90fe977f2203feb5fee8
FernandaAllo <fernandasouzalimaallo@gmail.com>
2026-04-15 20:39:44 -0300

    componente da imagem do card e genero do card

---
+++ src/components/CardImagem.jsx
@@ -0,0 +1,14 @@
+import CardGenero from "./CardGenero"
+
+const CardImagem = ({imagem, alt}) => {
+    return(
+                <img
+                    src={imagem || 'https://placehold.co/600x400?text=Imagem+não+encontrada'}
+                    alt={alt}
+                    className="w-full h-full object-cover cursor-zoom-in hover:scale-105 transition-transform duration-500"
+                    onClick={() => setImagemAberta(true)}
+                />
+    )
+}
+
+export default CardImagem
