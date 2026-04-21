d6d30417c6fc90d2d3fe55fed6bc9fcb421d3802
DS <1-25-13173@aluno.faminas.edu.br>
2026-04-21 16:23:02 -0300

    Reposiocionamento do gênero no Card (canto superior para canto inferior)

--- src/components/CardGenero.jsx
+++ src/components/CardGenero.jsx
@@ -1,6 +1,6 @@
 const CardGenero = ({ genero }) => {
     return (
-        <span className="absolute top-3 right-3 bg-blue-600 text-white text-xs font-bold px-2 py-1 rounded-full uppercase">
+        <span className="absolute bottom-3 right-2 bg-blue-600 text-white text-xs font-bold px-2 py-1 rounded-full uppercase">
             {(genero).join(" / ") || "Nenhum gênero disponível"}
         </span>
     )
